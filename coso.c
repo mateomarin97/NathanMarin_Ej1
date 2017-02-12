7df3ac36fcb8ef74326d1772f223a4cc012bb1cd
NestorJauregui <ni.jauregui10@gmail.com>
2017-02-12 14:18:25 -0500

    Veamos que pasa

--- coso.c
+++
@@ -1,4 +0,0 @@
-int main(int argc, char const *argv[]) {
-  write("sjdfksjdfks\n");
-  return 0;
-}
--- numlineas.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-with open('datos.dat') as f:
-    lines = f.readlines()
-
-for i in lines:
-    print(len(i.split(" ")))
